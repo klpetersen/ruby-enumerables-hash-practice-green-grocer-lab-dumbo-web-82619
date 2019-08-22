b2a23cf67ee5b40d8b1c2cefdae33ce154885ef3
Kayla Petersen <petersen1994@gmail.com>
2019-08-22 14:55:15 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -47,7 +47,7 @@ end
 
 def checkout(cart, coupons)
 cart = consolidate_cart(cart) 
-  cart = apply_coupons(cart, coupons)
+cart = apply_coupons(cart, coupons)
 cart = apply_clearance(cart) 
 total = 0 
 
